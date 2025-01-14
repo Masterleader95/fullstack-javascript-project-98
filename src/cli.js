dc98b10b9c8c38da33d9b3b000f35c1d33555f92
Masterleader95 <andersonarleychala@gmail.com>
2025-01-14 17:42:29 +0000

    3 paso completado

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export default function greetUser () {
+
+console.log('¡Bienvenido a Brain Games!');
+
+const name = readlineSync.question('¿Cuál es tu nombre? ');
+
+console.log(`¡Hola, ${name}!`);
+
+};
