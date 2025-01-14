06798b7862594781e07248c4f22a540b47fc032d
Masterleader95 <andersonarleychala@gmail.com>
2025-01-14 22:30:59 +0000

    5 paso del proyecto

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/2638f34fb2c8eb643982/maintainability)](https://codeclimate.com/github/Masterleader95/fullstack-javascript-project-98/maintainability)
 
+# Grabacion asciinema brain-even
+
+![asciicast](https://asciinema.org/connect/ad0187d7-cb71-414a-ad55-da5003eac189)
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Masterleader95/fullstack-javascript-project-98/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Masterleader95/fullstack-javascript-project-98/actions)
---
+++ bin/brain-even.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+//Función para generar un número aleatorio entre min y max.
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+//Función principal para ejecutar el juego "Verificación de paridad".
+
+const runParityGame = () => {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('¿Cuál es tu nombre? ');
+  console.log(`¡Hola, ${name}!`);
+  console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
+
+  const totalRounds = 3;
+  let correctAnswers = 0;
+
+  while (correctAnswers < totalRounds) {
+    const number = getRandomInt(1, 100);
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Pregunta: ${number}`);
+    const userAnswer = readlineSync.question('Tu respuesta: ').toLowerCase();
+
+    if (userAnswer === correctAnswer) {
+      console.log('¡Correcto!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
+      console.log(`¡Intentémoslo de nuevo, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`¡Felicidades, ${name}!`);
+};
+
+runParityGame();
